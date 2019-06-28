42d1633afb285e348e4f7f88edcf744ad733a9cb
ayushfreak <ayushaggarwal.97@gmail.com>
2019-06-28 15:39:21 +0530

    testing PEP8speaks

--- test/test_pyre/test.py
+++ test/test_pyre/test.py
@@ -1,10 +1,12 @@
 
 from typing import Any
 
-
+a = 5
 def foo() -> int:
     return test(123)  # type error: expected `int` but got `Any`
 
+if 1===:
+    print("will not work")
 
 def test(num: int) -> Any:
     print(num)
